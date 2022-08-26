7ef9a4c2915cd46d87d5bec4566260b59df82fcd
Mishor Patra <mishorpatra1608@gmail.com>
2022-08-26 14:52:32 +0530

    user location fetched

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from "react-native";
-import { WebView } from 'react-native-webview';
-import html_script from './html_script';
-
-
-//components
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 import GlobalView from './components/GlobalView';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  /*if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log(location.coords)
+  }*/
+
   return (
-    <GlobalView />
+    
+      <GlobalView coords={location.coords}  />
   );
 }
-
--- components/GlobalView.js
+++ components/GlobalView.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import {WebView} from 'react-native-webview'
 import { View, SafeAreaView } from 'react-native'
 
-export default function GlobalView() {
+export default function GlobalView({ coords }) {
+  console.log(coords)
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, marginTop: 25 }}>
         <WebView 
-          source={{ uri: 'https://inclunav.apps.iitd.ac.in/exibition-new/' }} 
+          source={{ uri: `http://10.194.6.249:3000/exibition-new?userlat=${coords.latitude}?userlng=${coords.longitude}/` }} 
         />
       </SafeAreaView>
     );
